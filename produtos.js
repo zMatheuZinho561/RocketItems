69415a30b3066237965e22fff573b760613e2a06
zMatheuZinho561 <matheuscardoso003@gmail.com>
2021-03-24 18:13:16 -0300

    first commit

---
+++ produtos.js
@@ -0,0 +1,131 @@
+const produtos = [
+
+
+{
+    id: '1',
+    image: './produtos img/hanbi.png',
+    title: 'Carro Harbinger GXT',
+    value: '40',
+},
+{
+    id: '2',
+    image: './produtos img/ronnon.png',
+    title: 'Carro Ronin GXT',
+    value: '90',
+    
+},
+{
+    id: '3',
+    image: './produtos img/fennec.jpg',
+    title: '?',
+    value: 'indisponivel',
+},
+{
+    id: '4',
+    image: './produtos img/chika.webp',
+    title: ' Carro Chikara GXT',
+    value: '90',
+},
+{
+    id: '5',
+    image: './produtos img/coma liquida.jpg',
+    title: 'Adesivo Camo Liquida',
+    value: '40',
+},
+{
+    id: '6',
+    image: './produtos img/compoc.jpg',
+    title: 'Adesivo Composto C',
+    value: '20',
+},
+{
+    id: '7',
+    image: './produtos img/luster ed.jpg',
+    title: 'Adesivo Luster Edge',
+    value: '90',
+},
+{
+    id: '8',
+    image: './produtos img/duna.jpg',
+    title: ' Adesivo Patrulha Das Dunas',
+    value: '40',
+},
+{
+    id: '9',
+    image: './produtos img/blss.png',
+    title: 'Roda Blssm',
+    value: '40',
+},
+{
+    id: '10',
+    image: './produtos img/cyber.png',
+    title: 'Roda Cyberware',
+    value: '20',
+},
+{
+    id: '11',
+    image: './produtos img/curto.jpg',
+    title: 'Roda Curto-Circuito',
+    value: '10',
+},
+
+{
+    id: '12',
+    image: './produtos img/dimonix.png',
+    title: 'Roda Dimonix',
+    value: '90',
+},
+{
+    id: '13',
+    image: './produtos img/garoto.jpg',
+    title: 'Roda Garoto-Músculos',
+    value: '90',
+},
+{
+    id: '14',
+    image: './produtos img/hamster.png',
+    title: 'Roda Hamster',
+    value: '30',
+},
+{
+    id: '15',
+    image: './produtos img/heli.webp',
+    title: 'Roda Helicoprion',
+    value: '20',
+},
+{
+    id: '16',
+    image: './produtos img/jandertek.png',
+    title: 'Roda Jandertek',
+    value: '20',
+},
+{
+    id: '17',
+    image: './produtos img/helice.jpg',
+    title: 'Roda Hélice',
+    value: '30',
+},
+{
+    id: '18',
+    image: './produtos img/holos.png',
+    title: 'Roda Holosphere',
+    value: '50',
+},
+{
+    id: '19',
+    image: './produtos img/mandala.jpg',
+    title: 'Roda Mandala',
+    value: '40',
+},
+
+
+
+
+
+
+
+
+
+
+
+]
